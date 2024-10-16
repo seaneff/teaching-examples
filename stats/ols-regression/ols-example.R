5d84fa3392180f07d7d1f7a0d2b71be67f8b32aa
seaneff <steph.eaneff@gmail.com>
2024-10-16 10:05:56 -0700

    update example

--- stats/ols-regression/ols-example.R
+++ stats/ols-regression/ols-example.R
@@ -1,5 +1,5 @@
 #####################################################
-## Reference ########################################
+## References #######################################
 #####################################################
 
 ## Some code based on lab from Elements of Statistical Learning
@@ -16,6 +16,7 @@ library(MASS) ## large collection of datasets and functions
 library(ISLR2) ## for datasets: https://hastie.su.domains/ISLR2/Labs/Rmarkdown_Notebooks/Ch3-linreg-lab.html
 library(wooldridge) ## for datasets: https://rdrr.io/cran/wooldridge/man/hprice1.html
 library(broom) ## for augment function
+library(scales) ## to get commas on y axis
 
 #####################################################
 ## Housing prices ###################################
@@ -23,10 +24,17 @@ library(broom) ## for augment function
 
 ## hprice dataset
 head(hprice1)
+View(hprice1)
 
 #####################################################
 ## Explore dataset  #################################
 #####################################################
+
+## for data dictionary
+?hprice1
+
+## basic summary stats for each field/column
+summary(hprice1)
 
 ## Are there variables that would not be valid outcomes for an OLS regression? If so, why?
 ## The colonial variable would not be suitable for OLS given that it is a binary variable. 
@@ -67,12 +75,14 @@ hprice1 %>%
 #####################################################
 
 ## base r
-cor(hprice1$price, hprice1$sqrft)
+## person is default method, but good to be explicit here
+cor(hprice1$price, hprice1$sqrft, method = "pearson")
 
 ## with pipes
+## person is default method, but good to be explicit here
 hprice1 %>% 
   summarise(
-    calculated_cor = cor(sqrft, price)
+    calculated_cor = cor(sqrft, price, method = "pearson")
   )
 
 #####################################################
@@ -94,6 +104,8 @@ hprice1 %>%
 lm.fit <- lm(price ~ sqrft, data = hprice1)
 summary(lm.fit)
 
+## Is the coefficient the same as what we calculated "by hand"?
+
 #####################################################
 ## Create a scatterplot that shows regression #######
 #####################################################
@@ -105,17 +117,34 @@ hprice1 %>%
   labs(y = "Home price (USD)",
        x = "Home square footsage",
        title = "Home price vs. square footage") +
-  geom_smooth(method = 'lm', se = FALSE)
+  geom_smooth(method = 'lm', se = FALSE) +
+  scale_y_continuous(labels = scales::dollar_format(prefix="$"))
 
 #####################################################
 ## Assess residuals #################################
 #####################################################
 
 ## extract residuals
-hprice1_augmented <- augment(lm.fit)
+hprice1_augmented <- cbind.data.frame(augment(lm.fit), 
+                                      colonial = hprice1$colonial)
 head(hprice1_augmented)
 
+## what do the residuals sum to?
+## what do you expect?
+sum(hprice1_augmented$.resid)
+summary(hprice1_augmented$.resid)
+
+## plot residuals, histogram
+hprice1_augmented %>% 
+  ggplot() + 
+  aes(x = .resid) + 
+  geom_histogram(color = "black", fill = "#605678", bins = 20) +
+  labs(y = "Frequency",
+       x = "Residuals",
+       title = "Distribution of residuals") 
+
 ## plot residuals, scatterplot
+## residuals vs. fitted values
 hprice1_augmented %>% 
   ggplot() + 
   aes(y = .resid, x = .fitted) + 
@@ -123,6 +152,30 @@ hprice1_augmented %>%
   labs(y = "Residuals",
        x = "Fitted Values",
        title = "Residuals vs. Fitted") 
+
+## plot residuals, scatterplot
+## residuals vs. fitted values
+## explore impact of another features not in the model
+hprice1_augmented %>% 
+  ggplot() + 
+  aes(y = .resid, x = .fitted, color = factor(colonial == 1)) + 
+  geom_point() +
+  labs(y = "Residuals",
+       x = "Fitted Values",
+       color = "Colonial?",
+       title = "Residuals vs. Fitted") 
+
+
+#################################################################
+## Add additional coefficients ##################################
+#################################################################
+
+## Estimate a new model (and save it into another object) that includes the size of the lot and whether the house is a colonial.
+## BEFORE YOU DO, make a prediction: What do you think is going to happen to the coefficient that relates square footage and price?
+## Will the coefficient increase, decrease, or stay the same? Why?
+  
+summary(lm(price ~ sqrft, data = hprice1))
+summary(lm(price ~ sqrft + lotsize + colonial, data = hprice1))
 
 
 #################################################################
@@ -165,22 +218,22 @@ Boston %>%
 ## with medv as the response and lstat as the predictor. The basic syntax is lm(y ~ x, data), 
 ## where y is the response, x is the predictor, and data is the data set in which these two variables are kept.
 
-lm.fit <- lm(medv ~ lstat, data = Boston)
+lm.fit.2 <- lm(medv ~ lstat, data = Boston)
 
 ## print out full model object
-lm.fit
+lm.fit.2
 
 ## check out the data type
-is(lm.fit)
+is(lm.fit.2)
 
 ## look under the hood at what the object contains
-names(lm.fit)
+names(lm.fit.2)
 
 ## extract just the coefficients
-coef(lm.fit)
+coef(lm.fit.2)
 
 ## extract just the residuals
-residuals(lm.fit)
+residuals(lm.fit.2)
 
 #####################################################
 ## Visualize model ##################################
@@ -193,7 +246,7 @@ Boston %>%
   labs(y = "Median house value (USD)",
        x = "% households with low socioeconomic status",
        title = "Socioeconomic status and home values") +
-  scale_y_continuous(labels = scales::percent) +
+  scale_x_continuous(labels = scales::percent) +
   geom_smooth(method = 'lm')
 
 #####################################################
@@ -201,7 +254,7 @@ Boston %>%
 #####################################################
 
 ## extract residuals
-boston_augmented <- augment(lm.fit)
+boston_augmented <- augment(lm.fit.2)
 head(boston_augmented)
 
 ## what do you think residuals will sum to?
@@ -227,3 +280,21 @@ boston_augmented %>%
 
 par(mfrow = c(2, 2))
 plot(lm.fit)
+
+#####################################################
+## What if we add a quadratic term? #################
+#####################################################
+
+Boston %>% 
+  ggplot() + 
+  aes(y = medv, x =  lstat/100) + 
+  geom_point() +
+  labs(y = "Median house value (USD)",
+       x = "% households with low socioeconomic status",
+       title = "Socioeconomic status and home values") +
+  scale_x_continuous(labels = scales::percent) +
+  stat_smooth(method = "lm", formula = y ~ x + I(x^2))
+
+## How do you think that residuals would look?
+## What are the risks of adding additional terms?
+## What are the benefits?
