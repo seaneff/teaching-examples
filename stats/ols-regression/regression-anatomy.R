d1589ca15ddc99beaa5b1b9c33cf623c4bbbdcd1
seaneff <steph.eaneff@gmail.com>
2024-10-16 14:42:18 -0700

    add a bit more context

--- stats/ols-regression/regression-anatomy.R
+++ stats/ols-regression/regression-anatomy.R
@@ -17,10 +17,11 @@ library(broom) ## for augment function
 ## Simulate data ####################################
 #####################################################
 
+set.seed(532)
 d <- data.frame(
-  x1 = runif(n=100, min=0, max=10), 
-  x2 = runif(n=100, min=0, max=10), 
-  x3 = runif(n=100, min=0, max=10)
+  x1 = runif(n = 100, min = 0, max = 10), 
+  x2 = runif(n = 100, min = 0, max = 10), 
+  x3 = runif(n = 100, min = 0, max = 10)
 )
 
 ## because we know the population model, we can produce a single sample from it 
@@ -31,11 +32,11 @@ d <- d %>%
 
 head(d)
 
-
 #####################################################
 ## Fit first model ##################################
 #####################################################
 
+## initial form of model
 model_main <- lm(y ~ x1 + x2 + x3, data = d)
 coef(model_main)
 
@@ -43,11 +44,12 @@ coef(model_main)
 ## Fit first auxillary model ########################
 #####################################################
 
-## fit model
+## fit model predicting x1 from other xi values
 model_aux <- lm(x1 ~ x2 + x3, data = d)
 coef(model_aux)
 
-## extract residuals
+## extract residuals:
+## the unique variation of x1 -- the part you can't explain with the other Xs
 d_augmented <- augment(model_aux)
 d_augmented$y <- d$y
 head(d_augmented)
@@ -63,8 +65,16 @@ coef(model_two)
 ## Fit second auxillary model #######################
 #####################################################
 
-coef(model_two)
-coef(model_main)
+coef(model_main) ## B_1 = 0.9614521
+coef(model_two) ## B_1 = 0.9614521
 
 ## coefficient of the residuals in the second auxillary model
-## is equilvant to the coefficient of x1 in our first model
+## is equivalent to the coefficient of x1 in our first model
+## wow
+
+## and also equal! 0.9614521
+## the math maths!
+cov(d_augmented$y, d_augmented$.resid)/var(d_augmented$.resid)
+
+## why is this not equal to 1 (set in generating the data?)
+summary(model_main)
