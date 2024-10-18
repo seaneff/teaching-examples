0aad77c7e6ac583de5ddb6073bfff8357850fb5f
seaneff <steph.eaneff@gmail.com>
2024-10-17 19:26:51 -0700

    add logistic regression data

---
+++ stats/logistic-regression/README.md
@@ -0,0 +1,8 @@
+## Dataset Details
+
+**Citation** Holt M, MacGibbon J, Smith A, Broady T, Davis M, Newman C. Trust in Digital Health dataset. Zenodo; 2022. 
+Obtained online from [Zenodo](https://zenodo.org/records/7348017) on November 17, 2023.
+License: Creative Commons Zero v1.0 Universal 
+
+"The Trust in Digital Health project was conducted by the Centre for Social Research in Health, UNSW Sydney in collaboration with community organisations to assess views of digital health systems in Australia, particularly among communities affected by bloodborne viruses and sexually transmissible infections. We conducted a national, online survey of Australians' attitudes to digital health in April–June 2020. The sample (N=2,240) was recruited from the general population and four priority populations affected by HIV and other sexually transmissible infections: gay and bisexual men, people living with HIV, sex workers, and trans and gender-diverse people. The deidentified dataset and syntax provided here were used for an analysis of factors associated with greater knowledge of My Health Record and the likelihood of opting out of the system. My Health Record is Australia's national, digital, personal health record system."
+
---
+++ stats/logistic-regression/logistic-regression-example.R
@@ -0,0 +1,32 @@
+#######################################################################
+### Background ########################################################
+#######################################################################
+
+# Holt M, MacGibbon J, Smith A, Broady T, Davis M, Newman C. Trust in Digital Health dataset. Zenodo; 2022. 
+## License: Creative Commons Zero v1.0 Universal 
+
+## Holt M, MacGibbon J, Smith AK, Broady TR, Davis MD, Newman CE. Knowledge of Australia’s my Health Record
+## and factors associated with opting out: results from a national survey of the Australian general population 
+## and communities affected by HIV and sexually transmissible Infections. PLOS Digital Health. 2023 Mar 1;2(3):e0000200.
+
+## Data collection details 
+# "The Trust in Digital Health project was conducted by the Centre for Social Research in Health, 
+# UNSW Sydney in collaboration with community organisations to assess views of digital health systems 
+# in Australia, particularly among communities affected by bloodborne viruses and sexually transmissible 
+# infections. We conducted a national, online survey of Australians' attitudes to digital health in 
+# April–June 2020. The sample (N=2,240) was recruited from the general population and four priority 
+# populations affected by HIV and other sexually transmissible infections: gay and bisexual men, 
+# people living with HIV, sex workers, and trans and gender-diverse people. The deidentified dataset 
+# and syntax provided here were used for an analysis of factors associated with greater knowledge of My 
+# Health Record and the likelihood of opting out of the system. My Health Record is Australia's national, 
+# digital, personal health record system."
+
+## From the peer-reviewed publication
+## "Multivariate logistic regression was employed to identify independent associations with each outcome 
+## (knowledge of My Health Record and opting out or deleting the record). Covariates included demographic details, 
+## socioeconomic status, having chronic health conditions, self-reported health status [39], 
+## receiving mental health care, and membership of the targeted priority populations. 
+
+#######################################################################
+### Read in data ######################################################
+#######################################################################
